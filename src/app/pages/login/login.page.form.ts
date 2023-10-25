6c72e281661a2a4ea58f281b4c85ae7a7c7ebb04
idanCohenZ <101664812+idanCohenZ@users.noreply.github.com>
2023-10-25 19:43:08 +0300

    added store and basic actions for loader

--- src/app/pages/login/login.page.form.ts
+++ src/app/pages/login/login.page.form.ts
@@ -8,6 +8,8 @@ export class LoginPageForm {
   createForm(): FormGroup {
     //this function supplies an object that will tell us which fields will be in the form
     //and how they will be validated
+    //inside each field of the form there is:
+    //fieldName: [initial value, [Validators]]
     return this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
